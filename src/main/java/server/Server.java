e6cf853ff0ea98912b468c5b6971dae0fa816fb3
Plinio <AVZhykov@gmail.com>
2020-01-25 15:27:38 +0600

    Change method main in Server.class

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Server {
         public static void main(String[] args) {
-            SpringApplication.run(Server.class);
+            SpringApplication.run(Server.class, args);
         }
 }
